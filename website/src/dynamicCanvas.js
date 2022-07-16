dd7a25cb6527e4f2ea2dc925a04a60bc69ff571a
Kswooksw0 <58518544+Kswooksw0@users.noreply.github.com>
2022-07-16 11:16:57 +0800

    Add files via upload

---
+++ website/src/dynamicCanvas.js
@@ -0,0 +1,15 @@
+
+function preload() {
+	
+}
+function setup() {
+	let d = pixelDensity(2);
+	let cnv = createCanvas(windowWidth, windowHeight);
+	let body = document.querySelector("body");
+	cnv.parent(body);
+	
+}
+
+function draw() {}
+
+
